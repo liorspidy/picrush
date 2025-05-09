f0be2e0e66b7ae6ce36351cf8371200dd25fe7f9
Lior Fridman <76394602+liorspidy@users.noreply.github.com>
2025-05-09 22:33:20 +0300

    .

--- src/components/galleryBottomActions/GalleryBottomActions.tsx
+++ src/components/galleryBottomActions/GalleryBottomActions.tsx
@@ -1,5 +1,8 @@
 import type { SetStateAction } from 'react';
 import classes from './GalleryBottomActions.module.scss';
+import trashIcon from '@/assets/icons/trash.svg';
+import shareIcon from '@/assets/icons/share.svg';
+import whatsapp from '@/assets/icons/whatsapp.svg';
 
 interface GalleryBottomActionsProps {
     allowRemoving: boolean;
@@ -20,24 +23,37 @@ const GalleryBottomActions = ({
     setIsRemoving(true);
   };
 
+  const shareHandler = () => {
+    //
+  }
+
   return (
     <div className={classes.bottomActionsWrapper}>
-      {allowRemoving && (
-        <button
-          type="button"
-          className={`${classes.btn} ${classes.removeAll}`}
-          onClick={removeAllHandler}
-        >
-          Remove
-        </button>
-      )}
+      <div className={classes.roundBtnsWrapper}>
+        {allowRemoving && (
+          <button
+            type="button"
+            className={`${classes.roundBtn} ${classes.removeAll}`}
+            onClick={removeAllHandler}
+          >
+            <img className={classes.icon} src={trashIcon} alt='remove images' />
+          </button>
+        )}
+          <button
+            type="button"
+            className={`${classes.roundBtn} ${classes.shareViaWhatsapp}`}
+            onClick={shareHandler}
+          >
+            <img className={classes.icon} src={whatsapp} alt='share via whatsapp' />
+          </button>
+      </div>
 
       <button
         type="button"
         className={`${classes.btn} ${classes.deselectAll}`}
         onClick={deselectAllHandler}
       >
-        Deselect All
+        Canecl
       </button>
     </div>
   );
